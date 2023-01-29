d9dabf9368d7abfd1abd3cabcad304b622a5c1d4
MSc Luis A. Hernández Elias <luishe72@yahoo.com>
2023-01-28 22:09:39 -0300

    Configuracion de la conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import app from "./app.js";
+import { sequelize } from "./database/database.js";
 
-app.listen(4000);
-console.log("Server is listening on port 4000");
+async function main() {
+  try {
+    await sequelize.authenticate();
+
+    console.log("Se estableció la conexion con la base de datos");
+    app.listen(4000);
+    console.log("Server is listening on port 4000");
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+}
+
+main();
